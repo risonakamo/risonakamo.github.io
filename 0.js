9c6de8367dfb6c7d58ddbbaac16f769604016f88
risonakamo <hmykanae@gmail.com>
2019-01-04 16:08:26 -0500

    hold

--- 0.js
+++ 0.js
@@ -1,6 +1,8 @@
 //somewhere else.
 window.onload=(()=>{
-    var a=document.querySelector(".AREYOULOOKING");
-    document.querySelector(".MESSAGE").innerHTML=window.atob(a.innerText).split("\n")[Math.floor(Math.random()*(24))];
-    a.parentElement.removeChild(a);
+    setTimeout(()=>{
+        var a=document.querySelector(".AREYOULOOKING");
+        document.querySelector(".MESSAGE").innerHTML=window.atob(a.innerText).split("\n")[Math.floor(Math.random()*(24))];
+        a.parentElement.removeChild(a);
+    },Math.floor(Math.random()*1000)+1000);
 });
