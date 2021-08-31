0defbfd8136b4cc220006817c732a63589b77242
risona <hmykanae@gmail.com>
2021-08-31 00:29:30 -0400

    new site

--- index2/vlinks/array.py
+++
@@ -1,8 +0,0 @@
-infile=open("vlinks.txt","r");
-ofile=open("output","w");
-
-ofile.write("[");
-for x in infile:
-    ofile.write("\""+x[:-1]+"\",\n");
-ofile.seek(ofile.tell()-3);
-ofile.write("]");
